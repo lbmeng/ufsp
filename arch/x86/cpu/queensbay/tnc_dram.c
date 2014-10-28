ad66c346b50a7f79d1acb3ae28618f8e1b52c45d
Bin Meng <bmeng.cn@gmail.com>
2014-10-28 18:20:48 +0800

    tnc: Install BIOS E820 table for system memory layout
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/cpu/queensbay/tnc_dram.c
+++ arch/x86/cpu/queensbay/tnc_dram.c
@@ -13,6 +13,7 @@
 
 #include <common.h>
 #include <asm/fsp/fsp_support.h>
+#include <asm/e820.h>
 
 #define CROWNBAY_DRAM_SIZE	(1024 * 1024 * 1024)
 
@@ -37,4 +38,27 @@ int dram_init_banksize(void)
 ulong board_get_usable_ram_top(ulong total_size)
 {
 	return GetUsableLowMemTop(gd->arch.hob_list);
+}
+
+unsigned install_e820_map(unsigned max_entries, struct e820entry *entries)
+{
+	unsigned num_entries = 4;
+
+	entries[0].addr = 0;
+	entries[0].size = 0xa0000;
+	entries[0].type = E820_RAM;
+
+	entries[1].addr = 0xa0000;
+	entries[1].size = 0x60000;
+	entries[1].type = E820_RESERVED;
+
+	entries[2].addr = 0x100000;
+	entries[2].size = 0x3bf00000;
+	entries[2].type = E820_RAM;
+
+	entries[3].addr = 0x3c000000;
+	entries[3].size = 0x4000000;
+	entries[3].type = E820_RESERVED;
+
+	return num_entries;
 }
